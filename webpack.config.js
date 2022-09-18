174d0e419b9e9ef9d8a0f5fb49a0202fb56ccc1b
Tsai-Chieh <d0177381@gmail.com>
2022-09-18 20:44:00 +0800

    :wrench: tune output folder and template path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: prod ? 'production' : 'development',
   entry: './src/index.tsx',
   output: {
-    path: __dirname + '/dist/',
+    path: __dirname + '/build/',
   },
   module: {
     rules: [
@@ -28,7 +28,7 @@ module.exports = {
   devtool: prod ? undefined : 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: 'public/index.html',
     }),
     new MiniCssExtractPlugin(),
   ],
